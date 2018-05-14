8cbdc74881b6a7dd5e4b249327dd3f94163340e4
Hyeoncheol kim <hyeoncheolkim@Hyeoncheolui-MacBook-Pro-2.local>
2018-05-14 16:32:24 +0900

    타이핑 수정

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,15 @@
+export class 소득세Parameter {
+  과세지급액: number; 
+  부양가족수: number;
+  지급일: Date;
+}
+
+export class 일용직소득세Parameter {
+  총지급액: number; 
+  근로일수: number;
+}
+
 export namespace 간이세액표 {
-  export function get소득세({과세지급액: number, 부양가족수: number, 지급일: Date}): number;
-  export function get일용직소득세({ 총지급액: number, 근로일수: number }): number;
+  export function get소득세(param: 소득세Parameter): number;
+  export function get일용직소득세(param: 일용직소득세Parameter): number;
 }
